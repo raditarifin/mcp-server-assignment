67ac31f0d9e7a488a5f062f1636d1f5c7cbb8bfb
raditarifin <raditarifin82@gmail.com>
2025-07-09 16:07:35 +0700

    Add project files.

---
+++ MCPTools.cs
@@ -0,0 +1,27 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+
+namespace MCPServer.mcp_server
+{
+    [McpServerToolType]
+    public static class DayOfWeekTool
+    {
+        [McpServerTool, Description("Get the current day of the week")]
+        public static string GetDayOfWeek()
+        {
+            var today = DateTime.Now.ToString("dddd");
+            return $"Today is {today}.";
+        }
+    }
+
+    [McpServerToolType]
+    public static class MathTool
+    {
+        [McpServerTool, Description("Multiply two numbers")]
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
